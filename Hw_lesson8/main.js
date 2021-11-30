8b60fbbf979f9afc9d24b283e144d00cdcdc1bd3
Volodymyr Yahodka <volodymyryahodka@gmail.com>
2021-12-01 00:48:19 +0200

    hw_less8.

---
+++ Hw_lesson8/main.js
@@ -0,0 +1,49 @@
+// Hw_lesson8...
+// // Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+// //
+// // - Напишіть код,  який за допомоги document.getElementById або
+// // document.getElementsByClassName або document.getElementsByTagName :
+// // -- отримує текст з параграфа з id "content"
+// // let contentID = document.getElementById('content');
+// // console.log(contentID.innerText);
+// // // -- отримує текст з блоку з id "rules"
+// // let rules = document.getElementById('rules');
+// console.log(rules.innerText);
+//
+// // -- замініть текст параграфа з id 'content' на будь-який інший
+// // let contentP document.getElementById('content').innerText = 'asdqwe';
+//
+// // -- замініть текст параграфа з id 'rules' на будь-який інший;
+// // let pText = document.getElementById('rules').innerText = 'Okten is cool';
+// // -- змініть кожному елементу колір фону на червоний
+// // let children = document.body.children;
+// // for (const child of children) {
+// //     child.style.background = 'red ';
+// // }
+// // -- змініть кожному елементу колір тексту на синій
+// // let children = document.body.children;
+// // for (const child of children) {
+// //     child.style.color = 'blue';
+// // }
+// // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// // let element = document.getElementById('rules');
+// // console.log(element.classLits);
+//
+// // -- поміняти колір тексту у всіх елементів fc_rules на червоний
+//
+// // let colorText = document.getElementsByClassName('fc_rules');
+// // for ( const fcRule of fc_rules) {
+// //     fc_rules.style.background = 'red';
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
