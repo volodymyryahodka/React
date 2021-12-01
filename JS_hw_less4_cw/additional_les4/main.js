5e9bc68b67dd063e196b2edbdd69d792be47108c
Volodymyr Yahodka <volodymyryahodka@gmail.com>
2021-12-01 22:59:13 +0200

    hw_less4_add

---
+++ JS_hw_less4_cw/additional_les4/main.js
@@ -0,0 +1,90 @@
+// - створити функцію, яка якщо приймає один аргумент, просто вивдоить його,
+// якщо два аргументи - складає або конкатенує їх між собою.
+// function one (value) {
+//     console.log(value);
+// }
+// one('Okten is cool');
+// function concatString (name, surname) {
+//     console.log(name,surname);
+// }
+// concatString('petryk' + 'pyatochkin');
+
+// - створити функцію  яка приймає два масиви та скаладає значення
+// елементів з однаковими індексами  та повертає новий результуючий масив.
+// EXAMPLE:[1,2,3,4]--[2,3,4,5]----результат[3,5,7,9]
+// let arr1 = [1,2,3,4];
+// let arr2 = [2,3,4,5];
+// let rezult = [];
+// for (let i = 0; i < arr1.length; i++) {rezult[i] = arr1[i] + arr2[i]};
+// console.log(rezult);
+
+// - Створити функцію яка приймає масив будь яких объектів,
+// та повертає масив ключів всіх обєктів
+// EXAMPLE:[{name: 'Dima', age: 13}, {model: 'Camry'}] ==> [ name, age, model ];
+// let users = [{name: 'petya', age: 19}, {model: 'Avalon'}];
+// for (const user of  users) {
+//     for (const key in user) {
+//         console.log(key);
+//     }
+// }
+
+//  - Створити функцію яка приймає масив будь яких объектів,
+// та повертає масив значень всіх обєктів
+// EXAMPLE:[{name: 'Dima', age: 13}, {model: 'Camry'}] ==> [Dima, 13, Camry];
+// let users = [{name: 'Dima', age: 13}, {model: 'Camry'}] ;
+// for (const user of  users) {
+//     for (const key in user) {
+//         console.log(user[key]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
